73d07fda8c5ee6a0511eb7fd873c4fc57d4019f2
oleg_kharebov <xarebov1993@yandex.ru>
2023-01-08 13:44:20 +0300

    add dependencies injection

--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -6,8 +6,10 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        TestBean testBean = context.getBean("testBean", TestBean.class);
 
-        System.out.println(testBean.getName());
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        musicPlayer.playMusic();
+
+        context.close();
     }
 }

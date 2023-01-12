9bb2e138c23262fb2c72e54247730dfc6be190e7
oleg_kharebov <xarebov1993@yandex.ru>
2023-01-12 12:29:56 +0300

    lesson 9

--- src/main/java/org/example/RockMusic.java
+++ src/main/java/org/example/RockMusic.java
@@ -1,6 +1,22 @@
 package org.example;
 
-public class RockMusic implements Music{
+public class RockMusic implements Music {
+
+    private RockMusic() {
+    }
+
+    public static RockMusic getRockMusic(){
+        return new RockMusic();
+    }
+
+    public void doMyInit() {
+        System.out.println("initialization");
+    }
+
+    public void doMyDestroy() {
+        System.out.println("destructing");
+    }
+
     @Override
     public String getSong() {
         return "Wind cries Mary";
--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -6,15 +6,18 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
+        RockMusic rockMusic = context.getBean("rock", RockMusic.class);
+        System.out.println(rockMusic.getSong());
 
-        MusicPlayer musicPlayer1 = context.getBean("musicPlayer", MusicPlayer.class);
-        MusicPlayer musicPlayer2 = context.getBean("musicPlayer", MusicPlayer.class);
 
-        musicPlayer1.setVolume(15);
-        musicPlayer2.setVolume(25);
-
-        System.out.println(musicPlayer1.getVolume());
-        System.out.println(musicPlayer2.getVolume());
+//        MusicPlayer musicPlayer1 = context.getBean("musicPlayer", MusicPlayer.class);
+//        MusicPlayer musicPlayer2 = context.getBean("musicPlayer", MusicPlayer.class);
+//
+//        musicPlayer1.setVolume(15);
+//        musicPlayer2.setVolume(25);
+//
+//        System.out.println(musicPlayer1.getVolume());
+//        System.out.println(musicPlayer2.getVolume());
 
 
         context.close();
